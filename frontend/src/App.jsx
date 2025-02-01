bbe7701efa69060316e7a41408ff5ed0e0064a78
Faizan-313 <faizansyedpeer@gmail.com>
2025-02-01 21:04:47 +0530

    correct file name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "/src/assets/components/Header.jsx";
 import Main from "/src/assets/components/Main.jsx";
-import Footer from "/src/assets/components/footer";
+import Footer from "/src/assets/components/Footer.jsx";
 
 export default function App(){
   return (
