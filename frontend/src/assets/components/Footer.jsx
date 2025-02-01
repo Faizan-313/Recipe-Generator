aead62f7d7530b7562ffaa45c37d57b31db0db3f
Faizan-313 <faizansyedpeer@gmail.com>
2025-02-01 21:15:24 +0530

    correct the backend link

--- frontend/src/assets/components/Footer.jsx
+++ frontend/src/assets/components/Footer.jsx
@@ -9,12 +9,18 @@ function Footer() {
                 <p>&copy; 2025 <strong> Peer Faizan</strong>. All rights reserved.</p>
                 <ul className="socials">
                     <li>
-                        <a href="https://www.linkedin.com/in/faizan-syed-4370152b9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+                        <a 
+                            href="https://www.linkedin.com/in/faizan-syed-4370152b9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                            target='_blank'
+                        >
                             <img src={linkedInIcon} />
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/Faizan-313/Recipe-Generator">
+                        <a 
+                            href="https://github.com/Faizan-313/Recipe-Generator"
+                            target='_blank'
+                        >
                             <img id="github-image" src={githubIcon} />
                         </a>
                     </li>
